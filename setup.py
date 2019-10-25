45e0d9dc2de378526e4cb357670fd88933b9d981
Praneeth <praneethbedapudi@gmail.com>
2019-10-25 14:25:14 +0530

    update version

--- setup.py
+++ setup.py
@@ -18,12 +18,11 @@ URL = 'https://github.com/bedapudi6788/seqtag-keras'
 EMAIL = 'praneethbedapudi@gmail.com'
 AUTHOR = 'BEDAPUDI PRANEETH'
 REQUIRES_PYTHON = '>=3.5.0'
-VERSION = '1.0.3'
+VERSION = '1.0.4'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'Keras==2.2.4',
-    'tensorflow==1.12',
+    'Keras',
     'scikit-learn==0.19.1',
     'seqeval==0.0.3'
 ]
