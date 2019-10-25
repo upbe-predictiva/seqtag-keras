398008613b5aa0011305d8db75cb2de8321afb2b
Praneeth <praneethbedapudi@gmail.com>
2019-10-25 15:08:23 +0530

    fix crf_nll at trainng

--- seqtag_keras/layers.py
+++ seqtag_keras/layers.py
@@ -18,6 +18,32 @@ from keras.layers import InputSpec
 from keras.losses import categorical_crossentropy
 from keras.losses import sparse_categorical_crossentropy
 
+def crf_nll(y_true, y_pred):
+    """The negative log-likelihood for linear chain Conditional Random Field (CRF).
+    This loss function is only used when the `layers.CRF` layer
+    is trained in the "join" mode.
+    # Arguments
+        y_true: tensor with true targets.
+        y_pred: tensor with predicted targets.
+    # Returns
+        A scalar representing corresponding to the negative log-likelihood.
+    # Raises
+        TypeError: If CRF is not the last layer.
+    # About GitHub
+        If you open an issue or a pull request about CRF, please
+        add `cc @lzfelix` to notify Luiz Felix.
+    """
+
+    crf, idx = y_pred._keras_history[:2]
+    if crf._outbound_nodes:
+        raise TypeError('When learn_model="join", CRF must be the last layer.')
+    if crf.sparse_target:
+        y_true = K.one_hot(K.cast(y_true[:, :, 0], 'int32'), crf.units)
+    X = crf._inbound_nodes[idx].input_tensors[0]
+    mask = crf._inbound_nodes[idx].input_masks[0]
+    nloglik = crf.get_negative_log_likelihood(y_true, X, mask)
+    return nloglik
+
 def crf_loss(y_true, y_pred):
     """General CRF loss function depending on the learning mode.
     # Arguments
@@ -428,16 +454,16 @@ class CRF(Layer):
 
     @property
     def loss_function(self):
-        warnings.warn('CRF.loss_function is deprecated '
-                      'and it might be removed in the future. Please '
-                      'use losses.crf_loss instead.')
+        # warnings.warn('CRF.loss_function is deprecated '
+                    #   'and it might be removed in the future. Please '
+                    #   'use losses.crf_loss instead.')
         return crf_loss
 
     @property
     def accuracy(self):
-        warnings.warn('CRF.accuracy is deprecated and it '
-                      'might be removed in the future. Please '
-                      'use metrics.crf_accuracy')
+        # warnings.warn('CRF.accuracy is deprecated and it '
+                    #   'might be removed in the future. Please '
+                    #   'use metrics.crf_accuracy')
         if self.test_mode == 'viterbi':
             return crf_viterbi_accuracy
         else:
